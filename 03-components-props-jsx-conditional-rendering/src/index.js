5901a9e071db105af7512c0ad624bed19a74aef1
Yash Bhanderi <146318098+yash-bhanderi@users.noreply.github.com>
2024-12-13 23:41:50 +0530

    03-components-props-jsx-conditional-rendering

---
+++ 03-components-props-jsx-conditional-rendering/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Hello React!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
